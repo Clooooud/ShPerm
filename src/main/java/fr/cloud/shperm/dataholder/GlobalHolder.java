af914ebd49d8435425e065e22832d91b2da45ece
Clooooud <cloud38220ytb@hotmail.com>
2021-01-03 00:39:24 +0100

    Revert "Changement port par défaut + ajout modifier final"
    This reverts commit e2cb8c40

--- src/main/java/fr/cloud/shperm/dataholder/GlobalHolder.java
+++ src/main/java/fr/cloud/shperm/dataholder/GlobalHolder.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-public final class GlobalHolder {
+public class GlobalHolder {
 
     protected ShPerm plugin;
 
